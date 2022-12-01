e2053109af5e22cbb286712b95fb0074f4b2daaf
AngelinaShamanova <shamanovaaaa@yandex.ru>
2022-12-01 17:51:50 +0100

    Initial Commit

---
+++ BetterRestApp/BetterRestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestAppApp.swift
+//  BetterRestApp
+//
+//  Created by Ангелина Шаманова on 1.12.22..
+//
+
+import SwiftUI
+
+@main
+struct BetterRestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
